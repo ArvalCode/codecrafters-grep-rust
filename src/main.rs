f41096393673e3b9080a1d707279d571b2d06f65
Nolan Tran <ttrannol000@gmail.com>
2024-08-24 22:03:56 -0500

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,8 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
         let mut new_pattern = pattern.trim_matches('[').trim_matches(']').bytes();
         return input_line.bytes().any(|val| new_pattern.any(|p| val == p))
     } else if pattern.starts_with("[^") && pattern.ends_with(']') {
-        // negative character groups
-        let mut new_pattern = pattern.trim_matches('[').trim_matches(']').bytes();
-        return !input_line.bytes().any(|val| new_pattern.any(|p| val == p))
+        let cuttern = &pattern[2..pattern.len() - 1];
+        !input_line.chars().any(|c| cuttern.contains(c))
     }
     else {
         panic!("Unhandled pattern: {}", pattern)
