00e09f12244eba1c578b06a29f47b1b6f60d355d
Nolan Tran <ttrannol000@gmail.com>
2024-08-25 17:33:58 -0500

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,13 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
         let mut new_pattern = pattern.trim_matches('[').trim_matches(']').bytes();
         return input_line.bytes().any(|val| new_pattern.any(|p| val == p));
     } else if pattern.starts_with("[^") && pattern.ends_with(']') {
-        let chars_to_exclude: String = pattern[2..pattern.len()-1].to_string();
-        let exclude_chars: std::collections::HashSet<char> = chars_to_exclude.chars().collect();
-        input_line.chars().any(|c| exclude_chars.contains(&c))
+        if pattern.starts_with("[^") && pattern.ends_with(']') {
+            let chars_to_exclude: String = pattern[2..pattern.len()-1].to_string();
+            let exclude_chars: std::collections::HashSet<char> = chars_to_exclude.chars().collect();
+            
+            // Return true if none of the input line's characters are in the exclude set
+            !input_line.chars().any(|c| exclude_chars.contains(&c))
+        }
     } 
      else {
         panic!("Unhandled pattern: {}", pattern)
